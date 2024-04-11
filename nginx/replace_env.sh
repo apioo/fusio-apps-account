dc79e5fa0fcb97495dadca77a08c146e63529299
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-04-11 23:53:26 +0200

    fix replace base path

--- nginx/replace_env.sh
+++ nginx/replace_env.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 envsubst < /usr/share/index.html > /usr/share/nginx/html/index.html
-sed -i 's/developer\///g' /usr/share/nginx/html/index.html
+sed -i 's/account\///g' /usr/share/nginx/html/index.html
